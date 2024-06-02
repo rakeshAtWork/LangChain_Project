bd28fd6a0ca9ac9ae6ad699ced34379fb9902906
rakeshAtWork <153895722+rakeshAtWork@users.noreply.github.com>
2024-06-02 16:18:42 +0530

    Added ChatBot To shaperate Folder

--- ChatBot/main2.py
+++ ChatBot/main2.py
@@ -13,9 +13,6 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.llms import CTransformers
 from langchain.chains import RetrievalQA
 
-# Set your OpenAI API key
-# openai.api_key = os.getenv('OPENAI_API_KEY')  # Make sure to set this environment variable
-
 DB_FAISS_PATH = ""
 
 # custom_prompt_template = """Use the following pieces of information to answer the user's question.
@@ -27,18 +24,19 @@ DB_FAISS_PATH = ""
 # Only return the helpful answer below and nothing else.
 # Helpful answer:
 # """
-custom_prompt_template = """Use the following pieces of information to answer the user's question.
-If you don't know the answer, just say that you don't know, don't try to make up an answer.
 
+custom_prompt_template = """Use the following pieces of information to answer the user's question.
+your name is COZBOT.you are very good at greeting people in a helpful and respectful manner and give good responses when someone ends the conversation.
+please don't give any extra information as response apart from the user's question.
+If you don't know the answer, just say that you don't know in a respectful and helpful manner, don't try to make up an answer.
+ 
 Context: {context}
 Question: {question}
+ 
+Only return the helpful answer below and nothing else.
+Helpful answer:
 """
-# prompt_t=ChatPromptTemplate.from_messages(
-#     [
-#         ("system","You are a helpful assistant. Please respond to the user queries"),
-#         ("user","Question:{question}")
-#     ]
-# )
+
 
 def set_custom_prompt():
     """
@@ -60,17 +58,12 @@ def retrieval_qa_chain(llm, prompt, db):
                                        )
     return qa_chain
 
+
 # Loading the model
 def load_llm():
-    # Load the locally downloaded model here
-    # llm = CTransformers(
-    #     model = "TheBloke/Llama-2-7B-Chat-GGML",
-    #     model_type="llama",
-    #     max_new_tokens = 512,
-    #     temperature = 0.5
-    # )
     llm = Ollama(model='llama3')
     return llm
+
 
 # QA Model Function
 def qa_bot():
@@ -111,43 +104,22 @@ st.markdown(
     .main-content {
         padding-top: 50px;
     }
-   [data-testid="ScrollToBottomContainer"]{
-            background-image: url("https://www.cozentus.com/assets/images/globe.svg");
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-        }
+  
     </style>
     """,
     unsafe_allow_html=True,
 )
 
-# st.markdown(
-#     """
-#     <style>
-#     .fixed-title {
-#         position: fixed !important;
-#         top: 0 !important;
-#         width: 100% !important;
-#         background-color: rgba(255, 255, 255, 0.8) !important;
-#         z-index: 100 !important;
-#         padding: 10px !important;
-#         box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1) !important;
-#     }
-#     .main-content {
-#         padding-top: 80px !important;
-#     }
-#     [data-testid="ScrollToBottomContainer"]{
+# we can add Image but we have to find its compatability.
+
+#  [data-testid="ScrollToBottomContainer"]{
 #             background-image: url("https://www.cozentus.com/assets/images/globe.svg");
 #             background-size: cover;
 #             background-repeat: no-repeat;
 #             background-attachment: fixed;
 #         }
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
-# Fixed title
+
+
 st.markdown('<div class="fixed-title"><h1>CozBot</h1></div>', unsafe_allow_html=True)
 
 # Main content with padding
